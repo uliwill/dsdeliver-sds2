670a60f2dbd24f76ef03c7798a2d3db60926a7d3
Ulisses Willian Reichenbach <uliwill@gmail.com>
2021-01-20 21:46:32 -0300

    Fix mapboxtoken

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,7 +2,10 @@ import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
 const API_URL = process.env.REACT_APP_API_URL;
+
+// fix token
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
+
 
 export function fetchProducts() {
     return axios(`${API_URL}/products`)
